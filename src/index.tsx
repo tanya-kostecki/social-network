93477302aeef4ac86e695d3738aaf2aecf6048fc
tanya-kostecki <tanya.pavlova1294@gmail.com>
2024-05-17 13:36:19 +0200

    added store

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,40 @@
 import './index.css';
-import {state} from "./redux/state";
-import {rerenderEntireTree} from "./render";
+import ReactDOM from 'react-dom';
+import App, {DialogType, FriendType, MessageType, PostType} from './App';
+import {store} from "./redux/state";
+import {BrowserRouter} from "react-router-dom";
 
-rerenderEntireTree(state)
+type Props = {
+    profilePage: {
+        posts: PostType[]
+        newPostText: string
+    },
+    dialogsPage: {
+        dialogs: DialogType[]
+        messages: MessageType[]
+        newMessageText: string
+    },
+    sidebar: {
+        friends: FriendType[]
+    }
+}
+
+
+const rerenderEntireTree = (state: Props) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state} addPost={store.addPost.bind(store)} updatePostText={store.updatePostText.bind(store)}
+                 addMessage={store.addMessage.bind(store)}
+                 updateMessageText={store.updateMessageText.bind(store)}/>
+            {/*<App state={store.getState()} addPost={store.addPost.bind(store)} updatePostText={store.updatePostText.bind(store)}*/}
+            {/*     addMessage={store.addMessage.bind(store)}*/}
+            {/*     updateMessageText={store.updateMessageText.bind(store)}/>*/}
+        </BrowserRouter>,
+        document.getElementById('root')
+    );
+}
+rerenderEntireTree(store.getState())
+store.subscribe(() => rerenderEntireTree(store.getState()))
+
+// rerenderEntireTree()
+// store.subscribe(rerenderEntireTree)
--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,99 +1,194 @@
-import {rerenderEntireTree} from "../render";
 import {DialogType, FriendType, MessageType, PostType} from "../App";
 import {v1} from "uuid";
 
-export type StateType = {
+export const store = {
+    _state: {
 
-    profilePage: {
-        posts: PostType[]
-        newPostText: string
-    }
-    dialogsPage: {
-        messages: MessageType[]
-        newMessageText: string
-        dialogs: DialogType[]
-    }
-    sidebar: {
-        friends: FriendType[]
-    }
-
-}
-export const state: StateType = {
-
-    profilePage: {
-        posts: [
-            {id: '1', message: 'Hi', likesCount: 0},
-            {id: '2', message: 'How are you?', likesCount: 2},
-            {id: '3', message: 'What are you doing?', likesCount: 14},
-        ],
-        newPostText: ''
+        profilePage: {
+            posts: [
+                {id: '1', message: 'Hi', likesCount: 0},
+                {id: '2', message: 'How are you?', likesCount: 2},
+                {id: '3', message: 'What are you doing?', likesCount: 14},
+            ],
+            newPostText: ''
+        },
+        dialogsPage: {
+            messages: [
+                {id: '1', message: 'Hi'},
+                {id: '2', message: 'Hello'},
+                {id: '3', message: 'How are you?'}
+            ],
+            newMessageText: '', //
+            dialogs: [
+                {id: '1', name: 'Dimych'},
+                {id: '2', name: 'Andrey'},
+                {id: '3', name: 'Sveta'},
+                {id: '4', name: 'Sasha'},
+                {id: '5', name: 'Viktor'},
+                {id: '6', name: 'Valera'},
+                {id: '7', name: 'Igor'},
+            ],
+        },
+        sidebar: {
+            friends: [
+                {
+                    id: 1,
+                    name: 'Sveta',
+                    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo6JT6tYTm1-OGzcVBgpJwMQs7zyW67sw_X5X3pFxf2w&s'
+                },
+                {
+                    id: 2,
+                    name: 'Philipp',
+                    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo6JT6tYTm1-OGzcVBgpJwMQs7zyW67sw_X5X3pFxf2w&s'
+                },
+                {
+                    id: 3,
+                    name: 'Katja',
+                    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo6JT6tYTm1-OGzcVBgpJwMQs7zyW67sw_X5X3pFxf2w&s'
+                }
+            ]
+        }
     },
-    dialogsPage: {
-        messages: [
-            {id: '1', message: 'Hi'},
-            {id: '2', message: 'Hello'},
-            {id: '3', message: 'How are you?'}
-        ],
-        newMessageText: '', //
-        dialogs: [
-            {id: '1', name: 'Dimych'},
-            {id: '2', name: 'Andrey'},
-            {id: '3', name: 'Sveta'},
-            {id: '4', name: 'Sasha'},
-            {id: '5', name: 'Viktor'},
-            {id: '6', name: 'Valera'},
-            {id: '7', name: 'Igor'},
-        ],
+    getState () {
+        return this._state
     },
-    sidebar: {
-        friends: [
-            {
-                id: 1,
-                name: 'Sveta',
-                avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo6JT6tYTm1-OGzcVBgpJwMQs7zyW67sw_X5X3pFxf2w&s'
-            },
-            {
-                id: 2,
-                name: 'Philipp',
-                avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo6JT6tYTm1-OGzcVBgpJwMQs7zyW67sw_X5X3pFxf2w&s'
-            },
-            {
-                id: 3,
-                name: 'Katja',
-                avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo6JT6tYTm1-OGzcVBgpJwMQs7zyW67sw_X5X3pFxf2w&s'
-            }
-        ]
+    _callSubscriber () {
+        console.log('state is changed')
+    },
+    addPost ()  {
+        const newPost = {
+            id: v1(),
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        }
+        this._state.profilePage.posts.push(newPost)
+        this._state.profilePage.newPostText = ''
+        this._callSubscriber()
+    },
+    updatePostText (newPostText: string) {
+        this._state.profilePage.newPostText = newPostText
+        this._callSubscriber()
+    },
+    addMessage () {
+        const newMessage = {
+            id: v1(),
+            message: this._state.dialogsPage.newMessageText
+        }
+        this._state.dialogsPage.messages.push(newMessage)
+        this._state.dialogsPage.newMessageText = ''
+        this._callSubscriber()
+    },
+    updateMessageText (messageText: string) {
+        this._state.dialogsPage.newMessageText = messageText
+        this._callSubscriber()
+    },
+    subscribe (observer: () => void) {
+        this._callSubscriber = observer
     }
-
 }
 
-export const addPost = () => {
-    const newPost = {
-        id: v1(),
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    }
-    state.profilePage.posts.push(newPost)
-    state.profilePage.newPostText = ''
-    rerenderEntireTree(state)
-}
+//@ts-ignore
+window.store = store
 
-export const updatePostText = (newPostText: string) => {
-    state.profilePage.newPostText = newPostText
-    rerenderEntireTree(state)
-}
+// let rerenderEntireTree = () => {
+//     console.log('state is changed')
+// }
+// export type StateType = {
+//
+//     profilePage: {
+//         posts: PostType[]
+//         newPostText: string
+//     }
+//     dialogsPage: {
+//         messages: MessageType[]
+//         newMessageText: string
+//         dialogs: DialogType[]
+//     }
+//     sidebar: {
+//         friends: FriendType[]
+//     }
+//
+// }
+// export const state: StateType = {
+//
+//     profilePage: {
+//         posts: [
+//             {id: '1', message: 'Hi', likesCount: 0},
+//             {id: '2', message: 'How are you?', likesCount: 2},
+//             {id: '3', message: 'What are you doing?', likesCount: 14},
+//         ],
+//         newPostText: ''
+//     },
+//     dialogsPage: {
+//         messages: [
+//             {id: '1', message: 'Hi'},
+//             {id: '2', message: 'Hello'},
+//             {id: '3', message: 'How are you?'}
+//         ],
+//         newMessageText: '', //
+//         dialogs: [
+//             {id: '1', name: 'Dimych'},
+//             {id: '2', name: 'Andrey'},
+//             {id: '3', name: 'Sveta'},
+//             {id: '4', name: 'Sasha'},
+//             {id: '5', name: 'Viktor'},
+//             {id: '6', name: 'Valera'},
+//             {id: '7', name: 'Igor'},
+//         ],
+//     },
+//     sidebar: {
+//         friends: [
+//             {
+//                 id: 1,
+//                 name: 'Sveta',
+//                 avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo6JT6tYTm1-OGzcVBgpJwMQs7zyW67sw_X5X3pFxf2w&s'
+//             },
+//             {
+//                 id: 2,
+//                 name: 'Philipp',
+//                 avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo6JT6tYTm1-OGzcVBgpJwMQs7zyW67sw_X5X3pFxf2w&s'
+//             },
+//             {
+//                 id: 3,
+//                 name: 'Katja',
+//                 avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo6JT6tYTm1-OGzcVBgpJwMQs7zyW67sw_X5X3pFxf2w&s'
+//             }
+//         ]
+//     }
+//
+// }
 
-export const addMessage = () => {
-    const newMessage = {
-        id: v1(),
-        message: state.dialogsPage.newMessageText
-    }
-    state.dialogsPage.messages.push(newMessage)
-    state.dialogsPage.newMessageText = ''
-    rerenderEntireTree(state)
-}
+// export const addPost = () => {
+//     const newPost = {
+//         id: v1(),
+//         message: state.profilePage.newPostText,
+//         likesCount: 0
+//     }
+//     state.profilePage.posts.push(newPost)
+//     state.profilePage.newPostText = ''
+//     rerenderEntireTree()
+// }
 
-export const updateMessageText = (messageText: string) => {
-    state.dialogsPage.newMessageText = messageText
-    rerenderEntireTree(state)
-}
+// export const updatePostText = (newPostText: string) => {
+//     state.profilePage.newPostText = newPostText
+//     rerenderEntireTree()
+// }
+
+// export const addMessage = () => {
+//     const newMessage = {
+//         id: v1(),
+//         message: state.dialogsPage.newMessageText
+//     }
+//     state.dialogsPage.messages.push(newMessage)
+//     state.dialogsPage.newMessageText = ''
+//     rerenderEntireTree()
+// }
+
+// export const updateMessageText = (messageText: string) => {
+//     state.dialogsPage.newMessageText = messageText
+//     rerenderEntireTree()
+// }
+
+// export const subscribe = (observer: () => void) => {
+//     rerenderEntireTree = observer
+// }
