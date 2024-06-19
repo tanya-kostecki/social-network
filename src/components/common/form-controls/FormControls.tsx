bb25371d38821e509d77154b001b644686b5329d
tanya-kostecki <tanya.pavlova1294@gmail.com>
2024-06-19 20:51:23 +0200

    refactoring form controls

--- src/components/common/form-controls/FormControls.tsx
+++ src/components/common/form-controls/FormControls.tsx
@@ -2,22 +2,32 @@ import React from 'react';
 import styles from './FormControls.module.css'
 import {WrappedFieldProps} from "redux-form";
 
-type TextareaProps = {
+type FormControlProps = {
+    children: React.ReactNode
+    meta: {
+        touched: boolean
+        error?: string
+    }
+}
+const FormControl = ({ children, meta}: FormControlProps) => {
+    const hasError = meta.touched && meta.error
+    return (
+        <div className={styles.formControl + ' ' + (hasError ? styles.error : '')}>
+            {children}
+            {hasError && <span className={styles.errorSpan}>{meta.error}</span>}
+        </div>
+    );
+}
+
+type TextareaProps = WrappedFieldProps & {
     input: React.TextareaHTMLAttributes<HTMLTextAreaElement>;
     meta: {
         touched: boolean
         error?: string
     }
 };
-
 export const Textarea = ({ input, meta }: TextareaProps) => {
-    const hasError = meta.touched && meta.error
-    return (
-        <div className={styles.formControl + ' ' + (hasError ? styles.error : '')}>
-            <textarea {...input}/>
-            {hasError && <span className={styles.errorSpan}>{meta.error}</span>}
-        </div>
-    );
+    return <FormControl meta={meta}><textarea {...input}/></FormControl>
 };
 
 type InputProps = WrappedFieldProps & {
@@ -27,13 +37,6 @@ type InputProps = WrappedFieldProps & {
         error?: string
     }
 };
-
 export const Input = ({ input, meta }: InputProps) => {
-    const hasError = meta.touched && meta.error
-    return (
-        <div className={styles.formControl + ' ' + (hasError ? styles.error : '')}>
-            <input {...input}/>
-            {hasError && <span className={styles.errorSpan}>{meta.error}</span>}
-        </div>
-    );
+    return <FormControl meta={meta}><input {...input}/></FormControl>
 };
