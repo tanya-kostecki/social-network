bd5c0ef17583d664ba97ba70ab652b7b52808ddf
tanya-kostecki <tanya.pavlova1294@gmail.com>
2024-07-02 19:08:57 +0200

    updated pagination

--- src/components/users/Pagination.tsx
+++ src/components/users/Pagination.tsx
@@ -6,16 +6,39 @@ type PaginationProps = {
     totalUsersCount: number
     currentPage: number
     onPageChange: (page: number) => void
+    portionSize?: number
 };
-export const Pagination = ({totalUsersCount, pageSize, currentPage, onPageChange, }: PaginationProps) => {
-    let pagesCount = Math.ceil(totalUsersCount / pageSize)
-    let nextPage = currentPage + 1
-    let prevPage = currentPage - 1
+
+export const Pagination = ({totalUsersCount, pageSize, currentPage, onPageChange, portionSize = 10}: PaginationProps) => {
+    const pagesCount = Math.ceil(totalUsersCount / pageSize);
+    const pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
+    }
+
+    const portionCount = Math.ceil(pagesCount / portionSize);
+    const [portionNumber, setPortionNumber] = React.useState(Math.ceil(currentPage / portionSize));
+    const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+    const rightPortionPageNumber = portionNumber * portionSize;
+
     return (
         <div>
-            {prevPage >= 1 && <button onClick={() => onPageChange(prevPage)}>Назад</button>}
-            <span className={styles.selectedPage}>{currentPage}</span>
-            {nextPage <= pagesCount && <button onClick={() => onPageChange(nextPage)}>Вперед</button>}
+            {portionNumber > 1 && <button onClick={() => setPortionNumber(portionNumber - 1)}>Назад</button>}
+            {pages
+                .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+                .map(p => {
+                    return (
+                        <span
+                            key={p}
+                            className={p === currentPage ? styles.selectedPage : ''}
+                            onClick={() => onPageChange(p)}
+                            style={{cursor: 'pointer', padding: '0 5px'}}
+                        >
+                            {p}
+                        </span>
+                    );
+                })}
+            {portionCount > portionNumber && <button onClick={() => setPortionNumber(portionNumber + 1)}>Вперед</button>}
         </div>
     );
 };
